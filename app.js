ba4e64ec9128553c0ebbefa9eac29426799bb8c9
Bekzodbek <kodirov2000@list.ru>
2023-03-12 20:46:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2541,3 +2541,8 @@ coments */
 //   while (3 ** res < n) res++
 //   return res - 1
 // }
+
+// https://www.codewars.com/kata/582e4c3406e37fcc770001ad/train/javascript
+// function joinStrings(string1, string2){
+//   return `${string1} ${string2}`
+// }
